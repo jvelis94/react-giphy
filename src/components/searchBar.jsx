137b26fdf3035b9a6bce6661ef7257bd486f4e90
Joel Velis <joelvelis94@gmail.com>
2019-09-09 17:08:05 -0400

    gh-pages

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            term: ''
+        }
+    }
+
     handleUpdate = (event) => {
+        this.setState({
+            term: event.target.value
+        })
         this.props.searchGif(event.target.value)
     }
 
     render() {
         return (
-            <input type='text' className='form-control form-search'
-            onChange={this.handleUpdate} />
+            <input 
+                type='text' 
+                value={this.state.term}
+                className='form-control form-search'
+                onChange={this.handleUpdate} 
+            />
         )
     }
 }
